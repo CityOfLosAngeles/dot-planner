f5d478977a6c26e2d8420750c0526a03fdd27b8c
Colin McDaniel <colinmcdaniel@me.com>
2016-11-19 13:32:24 -0800

    Update Front End

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var port = 3000;
 
@@ -6,5 +7,8 @@ var morgan = require('morgan');
 
 app.use(express.static('./public'));
 
+app.use('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(3000);
