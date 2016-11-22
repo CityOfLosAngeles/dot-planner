7af12f0018ff4fbfbff74d6f74f723ca7d2f0205
Spencer Charest <SpencerC77@gmail.com>
2016-11-22 09:07:54 -0800

    so many stupid mistakes, fixed cookie-parser error

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
