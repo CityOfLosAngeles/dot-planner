ca8b7365b5e574b9f76d10effa563f4bdb4dd2a6
Spencer Charest <SpencerC77@gmail.com>
2016-11-22 09:05:19 -0800

    removed morgan and cookie-parser

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
