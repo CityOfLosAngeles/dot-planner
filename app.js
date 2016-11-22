950d87c3b0a68c477c5273f0e6925eaae687f356
Spencer Charest <SpencerC77@gmail.com>
2016-11-22 08:58:38 -0800

    testing sequelize-heroku package

--- app.js
+++ app.js
@@ -9,6 +9,30 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var sequelize = require('../index').connect();
+
+if (sequelize)
+{
+    sequelize.authenticate().then( function() {
+        var config = sequelize.connectionManager.config;
+        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
+
+        sequelize.query('SELECT 1+1 as test').then( function(res) {
+
+            console.log('1+1='+res[0].test);
+
+        });
+
+    }).catch( function(err) {
+        var config = sequelize.connectionManager.config;
+        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
+    });
+}
+else
+{
+    console.log('No environnement variable found.');
+}
+
 //MongoDB setup
 
 //local mongo instance
