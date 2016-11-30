69faadca8baec299341d7b47fa01c26062076267
Spencer Charest <SpencerC77@gmail.com>
2016-11-30 11:26:02 -0800

    separated view map and add project pages

--- public/javascripts/new-project.js
+++ public/javascripts/new-project.js
@@ -5,7 +5,6 @@ var newProject = {};
 var map = L.mapbox.map('map').setView([
     34.0522, -118.2437
 ], 14);
-map.invalidateSize();
 
 //If we want to include multiple map layers
 
@@ -14,6 +13,8 @@ map.invalidateSize();
 //     'Terrain Map': L.mapbox.tileLayer('bobbysud.i2pfp2lb', {detectRetina: true})
 // }).addTo(map);
 
+// TODO: Does mapbox API token expire? We probably need the city to make their own account and create a map. This is currently using Spencer's account.
+
 //Adding the underlying map layer to the map
 // L.tileLayer('https://api.mapbox.com/styles/v1/spencerc77/ciw309ms000ba2ko45wvaj6ay/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BlbmNlcmM3NyIsImEiOiJjaXczMDZ6NWwwMTgzMm9tbXR4dGRtOXlwIn0.TPfrEq5h7Iuain1LsBsC8Q', {detectRetina: true}).addTo(map);
 
@@ -21,27 +22,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/spencerc77/ciw30fzgs00ap2jpg6sj6ub
 
 //Adding a feature group to the map
 var featureGroup = L.featureGroup().addTo(map);
-
-//AJAX request to the PostgreSQL database to get all projects and render them on the map
-$.ajax({
-    type: 'GET',
-    url: '/projects',
-    datatype: 'JSON',
-    success: function(data) {
-        if (data) {
-            L.geoJson(data, {
-                //We can use this style option to style the render shapes however we'd like
-                style: function(feature) {
-                    return {"color": "#78c679", "weight": 5, "opacity": 0.9}
-                },
-                //Function to be run any time a feature is clicked. This one presents the popup with a little bit of project information
-                onEachFeature: function(feature, layer) {
-                    layer.bindPopup('<h1>UID: ' + feature.properties.UID + '</h1>' + '<h1>Title: ' + feature.properties.title + '</h1>' + '<h1>Description: ' + feature.properties.description + '</h1>');
-                }
-            }).addTo(map);
-        }
-    }
-});
 
 // //Setting up the Google search box
 // var GooglePlacesSearchBox = L.Control.extend({
@@ -78,39 +58,6 @@ $.ajax({
 //   });
 //
 // });
-
-//Example of how we can style the drawing tool and shapes as they are drawn
-var options = {
-    position: 'topleft',
-    draw: {
-        polyline: {
-            shapeOptions: {
-                color: '#f357a1',
-                weight: 10
-            }
-        },
-        polygon: {
-            allowIntersection: false, // Restricts shapes to simple polygons
-            drawError: {
-                color: '#e1e100', // Color the shape will turn when intersects
-                message: '<strong>Sorry!<strong>you can\'t draw that!' // Message that will show when intersect
-            },
-            shapeOptions: {
-                color: '#bada55'
-            }
-        },
-        circle: false, // We can turn off drawing tool options
-        rectangle: {
-            shapeOptions: {
-                clickable: false
-            }
-        }
-    },
-    edit: {
-        featureGroup: featureGroup, //REQUIRED!!
-        remove: false
-    }
-};
 
 //Add the drawing tool to the map passing in the above options as an argument
 var drawControlFull = new L.Control.Draw(options).addTo(map);
