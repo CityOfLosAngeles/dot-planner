88f2acad2b07116de631a9b815df6552dc65905a
Spencer Charest <SpencerC77@gmail.com>
2016-12-06 12:46:35 -0800

    drop tables

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+//sequelize sync
+var models = require('./models');
+models.sequelize.sync({force: true});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
