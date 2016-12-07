83808c6897baca724a69299177daf76aae9394f0
Spencer Charest <SpencerC77@gmail.com>
2016-12-07 14:17:25 -0800

    added project view section

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -121,6 +121,7 @@ $('#map-filter input').change(function() {
 function onEachFeature(feature, layer) {
   layer.on('click', function(e) {
     console.log(feature.properties);
+    //Common attributes
     $('#UID').text(feature.properties.UID);
     $('#Proj_Title').text(feature.properties.Proj_Title);
     $('#Proj_Desc').text(feature.properties.Proj_Desc);
@@ -132,5 +133,37 @@ function onEachFeature(feature, layer) {
     $('#Contact_info_email').text(feature.properties.Contact_info.Contact_info_email);
     $('#More_info').text(feature.properties.More_info);
     $('#CD').text(feature.properties.CD);
+
+    //Separate section for funded attributes
+    if (feature.properties.Fund_St === 'Funded') {
+      $('#unfunded-attributes').hide();
+      $('#Dept_Proj_ID').text(feature.properties.Dept_Proj_ID);
+      $('#Total_bgt').text('$' + feature.properties.Total_bgt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Grant').text('$' + feature.properties.Grant.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Other_funds').text('$' + feature.properties.Other_funds.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Prop_c').text('$' + feature.properties.Prop_c.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Measure_r').text('$' + feature.properties.Measure_r.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#General_fund').text('$' + feature.properties.General_fund.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Current_Status').text(feature.properties.Current_Status);
+      $('#Issues').text(feature.properties.Issues);
+      $('#Deobligation').text(feature.properties.Issues);
+      $('#Explanation').text(feature.properties.Explanation);
+      $('#Other_ID').text(feature.properties.Other_ID);
+      $('#Constr_by').text(feature.properties.Constr_by);
+      $('#Info_source').text(feature.properties.Info_source);
+
+      $('#funded-attributes').show();
+    } else {
+      $('#funded-attributes').hide();
+      //Separate section for unfunded attributes
+      $('#Grant_Cat').text(feature.properties.Grant_Cat);
+      $('#Proj_Ty').text(feature.properties.Proj_Ty);
+      $('#Est_Cost').text('$' + feature.properties.Est_Cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Fund_Rq').text('$' + feature.properties.Fund_Rq.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Lc_match').text('$' + feature.properties.Lc_match.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+      $('#Match_Pt').text(feature.properties.Match_Pt + '%');
+      $('#Comments').text(feature.properties.Comments);
+      $('#unfunded-attributes').show();
+    }
   });
 }
