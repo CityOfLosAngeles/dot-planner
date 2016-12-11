cde1113c877560acb1b1283de1c9c131ed821072
Spencer Charest <SpencerC77@gmail.com>
2016-12-11 09:58:07 -0800

    removed some unneccary files

--- ogr2ogr_example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Geodatabase to postgres
-ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 dbname=SampleNY user=postgres" path/to/geodatabase_file.gdb -overwrite -progress --config PG_USE_COPY YES
-
-# Shapefile to Geojson
-ogr2ogr -f GeoJSON -t_srs crs:84 [name].geojson [name].shp
-
-## To install ogr2ogr on a mac, run `brew install gdal`
