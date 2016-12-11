5ef12ea1879676ddf37ed88cf8cceb942c5ce8fd
Spencer Charest <SpencerC77@gmail.com>
2016-12-11 08:54:08 -0800

    finished fixing sequelize

---
+++ migrations/20161211165146-create-project.js
@@ -0,0 +1,129 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Geometry: {
+        type: Sequelize.JSONB
+      },
+      Fund_St: {
+        type: Sequelize.STRING
+      },
+      Legacy_ID: {
+        type: Sequelize.INTEGER
+      },
+      Lead_Ag: {
+        type: Sequelize.STRING
+      },
+      Proj_Title: {
+        type: Sequelize.STRING
+      },
+      Proj_Ty: {
+        type: Sequelize.STRING
+      },
+      Proj_Desc: {
+        type: Sequelize.STRING
+      },
+      Contact_info: {
+        type: Sequelize.JSONB
+      },
+      More_info: {
+        type: Sequelize.STRING
+      },
+      Primary_Street: {
+        type: Sequelize.STRING
+      },
+      Cross_Streets: {
+        type: Sequelize.JSONB
+      },
+      Project_Status: {
+        type: Sequelize.STRING
+      },
+      Proj_Man: {
+        type: Sequelize.STRING
+      },
+      CD: {
+        type: Sequelize.INTEGER
+      },
+      Access: {
+        type: Sequelize.STRING
+      },
+      Dept_Proj_ID: {
+        type: Sequelize.INTEGER
+      },
+      Total_bgt: {
+        type: Sequelize.DECIMAL
+      },
+      Grant: {
+        type: Sequelize.DECIMAL
+      },
+      Other_funds: {
+        type: Sequelize.DECIMAL
+      },
+      Prop_c: {
+        type: Sequelize.DECIMAL
+      },
+      Measure_r: {
+        type: Sequelize.DECIMAL
+      },
+      Gas_Tax: {
+        type: Sequelize.DECIMAL
+      },
+      General_fund: {
+        type: Sequelize.DECIMAL
+      },
+      Issues: {
+        type: Sequelize.STRING
+      },
+      Deobligation: {
+        type: Sequelize.STRING
+      },
+      Explanation: {
+        type: Sequelize.STRING
+      },
+      Other_ID: {
+        type: Sequelize.INTEGER
+      },
+      Constr_by: {
+        type: Sequelize.STRING
+      },
+      Info_source: {
+        type: Sequelize.STRING
+      },
+      Grant_Cat: {
+        type: Sequelize.STRING
+      },
+      Grant_Cycle: {
+        type: Sequelize.STRING
+      },
+      Est_Cost: {
+        type: Sequelize.DECIMAL
+      },
+      Fund_Rq: {
+        type: Sequelize.DECIMAL
+      },
+      Lc_match: {
+        type: Sequelize.DECIMAL
+      },
+      Match_Pt: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Projects');
+  }
+};
---
+++ models/project.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Project = sequelize.define('Project', {
+    Geometry: DataTypes.JSONB,
+    Fund_St: DataTypes.STRING,
+    Legacy_ID: DataTypes.INTEGER,
+    Lead_Ag: DataTypes.STRING,
+    Proj_Title: DataTypes.STRING,
+    Proj_Ty: DataTypes.STRING,
+    Proj_Desc: DataTypes.STRING,
+    Contact_info: DataTypes.JSONB,
+    More_info: DataTypes.STRING,
+    Primary_Street: DataTypes.STRING,
+    Cross_Streets: DataTypes.JSONB,
+    Proj_Status: DataTypes.STRING,
+    Proj_Man: DataTypes.STRING,
+    CD: DataTypes.INTEGER,
+    Access: DataTypes.STRING,
+    Dept_Proj_ID: DataTypes.STRING,
+    Other_ID: DataTypes.STRING,
+    Total_bgt: DataTypes.DECIMAL,
+    Grant: DataTypes.DECIMAL,
+    Other_funds: DataTypes.DECIMAL,
+    Prop_c: DataTypes.DECIMAL,
+    Measure_r: DataTypes.DECIMAL,
+    Gas_Tax: DataTypes.DECIMAL,
+    General_fund: DataTypes.DECIMAL,
+    Authorization: DataTypes.STRING,
+    Issues: DataTypes.STRING,
+    Deobligation: DataTypes.STRING,
+    Explanation: DataTypes.STRING,
+    Constr_by: DataTypes.STRING,
+    Info_source: DataTypes.STRING,
+    Grant_Cat: DataTypes.STRING,
+    Grant_Cycle: DataTypes.STRING,
+    Est_Cost: DataTypes.DECIMAL,
+    Fund_Rq: DataTypes.DECIMAL,
+    Lc_match: DataTypes.DECIMAL,
+    Match_Pt: DataTypes.INTEGER,
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Project;
+};
