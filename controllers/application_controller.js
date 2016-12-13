a6723f214f54d5e7e438f23a5e45ae8d20ae7aec
stephorpilla <stephanieorpilla@yahoo.com>
2016-12-12 20:35:49 -0800

    working login for users

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -1,10 +1,31 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('map');
+// map homepage
+router.get('/', function(req, res) {
+  	res.render('map', {
+	    logged_in: req.session.logged_in,
+	    adminclearance: req.session.adminclearance,
+	    id: req.session.user_id,
+	    email: req.session.email,
+	    firstname: req.session.firstname,
+	    lastname: req.session.lastname,
+	    phonenumber: req.session.phonenumber,
+	    admin: req.session.admin
+    });
+});
+
+//redirect to users sign in
+router.get('/signin', function(req, res) {
+  res.redirect('/users/signin');
+});
+
+//sign out destroys session
+router.get('/signout', function(req,res) {
+	req.session.destroy(function(err) {
+		res.redirect('/');
+	});
 });
 
 module.exports = router;
