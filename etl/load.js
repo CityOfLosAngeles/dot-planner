e6dc890cd43f3007846d0759ca96ffc71e4f605a
Eliot Reyes <eliotmij@gmail.com>
2017-01-11 10:42:34 -0800

    cleaned up folder

--- etl/load.js
+++ etl/load.js
@@ -1,5 +1,5 @@
 var models = require('../models/');
-var dataSet = require('./export_funded_lines.js');
+var dataSet = require('./combined_data.js');
 
 for (var i = 0; i < dataSet.length; i++) {
 	models.Project.create(dataSet[i]);
