ca8cd163584683583cd4bc39e196dcbda7e396fd
hunterowens <owens.hunter@gmail.com>
2017-02-13 19:24:33 -0800

    adding load script

---
+++ etl/load.js
@@ -0,0 +1,7 @@
+require("../data/atd_f_lines.geojson")
+require("../data/atd_f_multipoints.geojson")
+require("../data/atd_f_points.geojson")
+require("../data/atd_f_multipoints.geojson")
+require("../data/atd_uf_lines.geojson")
+require("../data/atd_uf_points.geojson")
+require("../data/atd_uf_polygon.geojson")
