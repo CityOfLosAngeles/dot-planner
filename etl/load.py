519f7d43762240db4af9d1f874d19e0c8c391aba
hunterowens <owens.hunter@gmail.com>
2017-02-21 11:24:14 -0800

    look at this nasty sequel query.

--- etl/load.py
+++ etl/load.py
@@ -24,6 +24,52 @@ file_names = [
              "atd_uf_points.geojson"
 ]
 
+# Load in the json
 data = {file_name: json_loader(file_name) for file_name in file_names}
 
+#load f_lines 
+for project in data['atd_f_lines']['features']:
+    cur.execute("""INSERT INTO Projects (Geometry,
+                                         Fund_St,
+                                         Legacy_ID,
+                                         Lead_Ag,
+                                         Proj_Title,
+                                         Proj_Ty,
+                                         Proj_Desc,
+                                         Contact_info,
+                                         More_info,
+                                         Primary_Street,
+                                         Cross_Streets,
+                                         Proj_Status,
+                                         Proj_Man,
+                                         CD,
+                                         Access,
+                                         Dept_Proj_ID,
+                                         Other_ID,
+                                         Total_bgt,
+                                         Grant,
+                                         Other_funds,
+                                         Prop_c,
+                                         Measure_r,
+                                         Gas_Tax,
+                                         General_fund,
+                                         Authorization,
+                                         Issues,
+                                         Deobligation,
+                                         Explanation,
+                                         Constr_by,
+                                         Info_source,
+                                         Grant_Cat,
+                                         Grant_Cycle,
+                                         Est_Cost,
+                                         Fund_Rq,
+                                         Lc_match,
+                                         Flagged,
+                                         Dup_ID) VALUES 
+                                         (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                                         """,
+                                         project['geometry'], 
+                                         "funded",
+                                         )
+                                         
 
