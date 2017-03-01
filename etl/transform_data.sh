39d6a6c1ba8fbda52b391a5ee9e95e4e2cb233af
hunterowens <owens.hunter@gmail.com>
2017-03-01 14:17:17 -0800

    etl updates

--- etl/transform_data.sh
+++ etl/transform_data.sh
@@ -4,6 +4,15 @@ set -e
 
 #unzip ATD_projectdatabase.gdb.zip
 
+# remove old geojson3
+
+if [ -f ../data/atd_f_lines.geojson ] ; then
+      rm ../data/*.geojson
+fi
+
+# remove temp tample 
+psql -d dot -c "DROP TABLE ogrgeojson"
+
 declare -a arr=("f_multipoints"
                 "f_points"
                 "f_lines"
@@ -17,8 +26,8 @@ for i in "${arr[@]}"
 do
   echo "$i"
   ogr2ogr -sql "SELECT * FROM $i" -f "GeoJSON" ../data/atd_$i.geojson ../data/ATD_projectdatabase.gdb
+  ogr2ogr -append -f "PostgreSQL" PG:"host=localhost user=dot-planner dbname=dot password=dot-planner" ../data/atd_$i.geojson
 done
 
-python load.py
-
+psql -d dot -c "SELECT * INTO Projects FROM ogrgeojson;"
 
