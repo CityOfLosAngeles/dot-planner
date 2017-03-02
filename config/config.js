833be9320c14e0e7709001382882b9022169b05e
hunterowens <owens.hunter@gmail.com>
2017-03-01 16:44:29 -0800

    updat config

--- config/config.js
+++ config/config.js
@@ -21,6 +21,8 @@ module.exports = {
         "host": process.env.RDS_HOSTNAME,
         "user": process.env.RDS_USERNAME,
         "password": process.env.RDS_PASSWORD,
-        "port": process.env.RDS_PORT
+        "port": process.env.RDS_PORT,
+        "dialect": "postgres",
+        "protocol": "postgres"
     }
 }
