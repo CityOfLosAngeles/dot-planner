eaa09098ec03ecd215ff57889fedc61698ef57f4
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-03-04 17:42:59 -0800

    Added toggle mad view

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,7 +5,7 @@ var geoJSON;
 var layerID;
 
 //Creating the map with mapbox (view coordinates are downtown Los Angeles)
-var map = L.mapbox.map('map');
+// var map = L.mapbox.map('map');
 
 var colors = {
     red: "#FF355E",
@@ -23,9 +23,29 @@ var colors = {
 
 // TODO: Does mapbox API token expire? We probably need the city to make their own account and create a map. This is currently using Spencer's account.
 
-L.tileLayer('https://api.mapbox.com/styles/v1/spencerc77/ciw30fzgs00ap2jpg6sj6ubnn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BlbmNlcmM3NyIsImEiOiJjaXczMDZ6NWwwMTgzMm9tbXR4dGRtOXlwIn0.TPfrEq5h7Iuain1LsBsC8Q', {
+// Creating the map with mapbox (view coordinates are downtown Los Angeles)
+var map = L.mapbox.map('map', {
+    layers: [mapBoxLayer, imageryLayer]
+});
+// TODO: Does mapbox API token expire? We probably need the city to make their own account and create a map. This is currently using Spencer's account.
+var mapBoxLayer = L.tileLayer("https://api.mapbox.com/styles/v1/spencerc77/ciw30fzgs00ap2jpg6sj6ubnn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BlbmNlcmM3NyIsImEiOiJjaXczMDZ6NWwwMTgzMm9tbXR4dGRtOXlwIn0.TPfrEq5h7Iuain1LsBsC8Q", {
+    id: 'sketch',
     detectRetina: true
-}).addTo(map);
+}),
+imageryLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    id: 'imagery',
+    //attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    detectRetina: true
+});
+var baseMaps = {
+    'sketch': mapBoxLayer,
+    'imagery': imageryLayer
+}
+L.control.layers(baseMaps, {}).addTo(map);
+
+L.tileLayer("https://api.mapbox.com/styles/v1/spencerc77/ciw30fzgs00ap2jpg6sj6ubnn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BlbmNlcmM3NyIsImEiOiJjaXczMDZ6NWwwMTgzMm9tbXR4dGRtOXlwIn0.TPfrEq5h7Iuain1LsBsC8Q", {
+    detectRetina: true
+}, {}).addTo(map);
 
 //Adding a feature group to the map
 var featureGroup = L.featureGroup().addTo(map);
