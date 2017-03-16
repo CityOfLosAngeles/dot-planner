1a09d2d852429082ced91944868fe3b3776c757b
PocketThi3f <krslai@yahoo.com>
2017-03-15 20:13:43 -0700

    small comment

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "development": {
         "username": "postgres",
-        "password": "password",
+        "password": "p0st4ge",
         "database": "postgres",
         "host": "127.0.0.1",
         "dialect": "postgres",
